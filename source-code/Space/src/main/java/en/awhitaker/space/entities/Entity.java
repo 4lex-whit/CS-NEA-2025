8e9c8650b64ff4e25ec2e11a0523f21c243b132e
4lex-whit <110124629+4lex-whit@users.noreply.github.com>
2024-11-12 11:49:58 +0000

    Add entity constructor to entity class

--- source-code/Space/src/main/java/en/awhitaker/space/entities/Entity.java
+++ source-code/Space/src/main/java/en/awhitaker/space/entities/Entity.java
@@ -1,5 +1,17 @@
 package en.awhitaker.space.entities;
 
 public abstract class Entity {
-
+	private String name;
+	
+	/**
+	 * Entity constructor.
+	 * @param name  the string that will be the entity's name
+	 * @throws IllegalArgumentException if the <code>name</code> parameter is blank
+	 */
+	public Entity(String name) throws IllegalArgumentException {
+		if (name.isBlank())
+			throw new IllegalArgumentException("Entity name cannot be blank.");
+		
+		this.name = name;
+	}
 }
