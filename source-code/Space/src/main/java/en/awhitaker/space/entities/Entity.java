b2c84cb9e153eeeab753f43ed6073546c3a3182d
4lex-whit <110124629+4lex-whit@users.noreply.github.com>
2024-11-12 11:54:47 +0000

    Add Entity.getName function

--- source-code/Space/src/main/java/en/awhitaker/space/entities/Entity.java
+++ source-code/Space/src/main/java/en/awhitaker/space/entities/Entity.java
@@ -5,7 +5,7 @@ public abstract class Entity {
 	
 	/**
 	 * Entity constructor.
-	 * @param name  the string that will be the entity's name
+	 * @param name the string that will be the entity's name
 	 * @throws IllegalArgumentException if the <code>name</code> parameter is blank
 	 */
 	public Entity(String name) throws IllegalArgumentException {
@@ -13,5 +13,12 @@ public abstract class Entity {
 			throw new IllegalArgumentException("Entity name cannot be blank.");
 		
 		this.name = name;
+	}
+	
+	/**
+	 * @return the name of the entity as a string
+	 */
+	public String getName() {
+		return name;
 	}
 }
