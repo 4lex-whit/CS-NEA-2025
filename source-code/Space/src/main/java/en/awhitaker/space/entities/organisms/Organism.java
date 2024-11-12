ba444da6694b41647d6df28f6ad6c969ba6b68d7
4lex-whit <110124629+4lex-whit@users.noreply.github.com>
2024-11-12 12:25:45 +0000

    Add organism constructor to organism class

--- source-code/Space/src/main/java/en/awhitaker/space/entities/organisms/Organism.java
+++ source-code/Space/src/main/java/en/awhitaker/space/entities/organisms/Organism.java
@@ -3,5 +3,26 @@ package en.awhitaker.space.entities.organisms;
 import en.awhitaker.space.entities.Entity;
 
 public abstract class Organism extends Entity {
+	private String name;
+	private int hp;
 	
+	/**
+	 * Organism constructor.
+	 *
+	 * @param name the string that will be the organism's name
+	 * @param hp   the hp of the organism
+	 * @throws IllegalArgumentException if:
+	 *     <ul>
+	 *         <li>the <code>name</code> parameter is blank</li>
+	 *         <li>the <code>hp</code> parameter is less than 1</li>
+	 *     </ul>
+	 */
+	public Organism(String name, int hp) throws IllegalArgumentException {
+		super(name);
+		
+		if (hp <= 0)
+			throw new IllegalArgumentException("HP cannot be less than 1.");
+		
+		this.hp = hp;
+	}
 }
