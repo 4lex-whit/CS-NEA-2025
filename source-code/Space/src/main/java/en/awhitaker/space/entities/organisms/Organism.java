715ea13a0aa6466a3cddac4fecf9d534f484d5f0
4lex-whit <110124629+4lex-whit@users.noreply.github.com>
2024-11-14 10:12:59 +0000

    Add maxHp to organism class

--- source-code/Space/src/main/java/en/awhitaker/space/entities/organisms/Organism.java
+++ source-code/Space/src/main/java/en/awhitaker/space/entities/organisms/Organism.java
@@ -3,25 +3,35 @@ package en.awhitaker.space.entities.organisms;
 import en.awhitaker.space.entities.Entity;
 
 public abstract class Organism extends Entity {
+	private int maxHp;
 	private int hp;
 	
 	/**
 	 * Organism constructor.
 	 *
-	 * @param name the string that will be the organism's name
-	 * @param hp   the hp of the organism
+	 * @param name  the string that will be the organism's name
+	 * @param maxHp the maximum hp of the organism
+	 * @param hp    the initial hp of the organism
 	 * @throws IllegalArgumentException if:
 	 *     <ul>
 	 *         <li>the <code>name</code> parameter is blank</li>
-	 *         <li>the <code>hp</code> parameter is less than 1</li>
+	 *         <li>the <code>maxHp</code> or <code>hp</code> parameters are less than 1</li>
+	 *         <li>the <code>hp</code> parameter is greater than the <code>maxHp</code> parameter</li>
 	 *     </ul>
 	 */
-	public Organism(String name, int hp) throws IllegalArgumentException {
+	public Organism(String name, int maxHp, int hp) throws IllegalArgumentException {
 		super(name);
+		
+		if (maxHp < 1)
+			throw new IllegalArgumentException("Max HP cannot be less than 1.");
 		
 		if (hp < 1)
 			throw new IllegalArgumentException("HP cannot be less than 1.");
 		
+		if (hp > maxHp)
+			throw new IllegalArgumentException("HP cannot be greater than max HP.");
+		
+		this.maxHp = hp;
 		this.hp = hp;
 	}
 }
